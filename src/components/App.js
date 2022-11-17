c23cb6612eb4b0c9d1569385b87a9255ad39bbff
Nisha Tanwar <nishatanwar2706@gmail.com>
2022-11-17 21:01:58 +0530

    remove useRef

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,24 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect} from "react";
 import "../styles/App.css";
 const App = () => {
   //code here
-  // const [name , setName] = useState("");
-  const name = useRef("");
+  const [name , setName] = useState("");
+  // const name = useRef("");
   const [finalName, setFinalName] = useState("");
 
   const handleInput = (e) => {
-    name.current = e.target.value;
-    // setName(e.target.value);
+    // name.current = e.target.value;
+    setName(e.target.value);
   };
   const handleClick = () => {
-    setFinalName(name.current);
-    // setFinalName(name);
+    // setFinalName(name.current);
+    setFinalName(name);
   };
 
   return (
     <div id="main">
-      <input id="input" ref={name} onChange={handleInput}></input>
-      {/* <input id="input" value={finalName} onChange={handleInput}></input> */}
+      {/* <input id="input" ref={name} onChange={handleInput}></input> */}
+      <input id="input" value={name} onChange={handleInput}></input>
       <button id="button" onClick={handleClick}>
         Click
       </button>
