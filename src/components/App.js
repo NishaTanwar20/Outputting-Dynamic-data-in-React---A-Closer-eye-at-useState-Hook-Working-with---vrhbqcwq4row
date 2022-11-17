7dde8b15570979fd9a618e737cce2757cbb241da
Nisha Tanwar <nishatanwar2706@gmail.com>
2022-11-17 21:08:28 +0530

    ref use

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,30 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect ,useRef} from "react";
 import "../styles/App.css";
 const App = () => {
   //code here
-  const [name , setName] = useState("");
-  // const name = useRef("");
-  const [finalName, setFinalName] = useState(name);
+  // const [name , setName] = useState("");
+  const name = useRef("");
+  const [finalName, setFinalName] = useState("_______");
 
   const handleInput = (e) => {
-    // name.current = e.target.value;
-    setName(e.target.value);
+    name.current = e.target.value;
+    // setName(e.target.value);
   };
   const handleClick = () => {
-    // setFinalName(name.current);
-    setFinalName(name);
+    setFinalName(name.current);
+    // setFinalName(name);
   };
 
   return (
     <div id="main">
-      {/* <input id="input" ref={name} onChange={handleInput}></input> */}
-      <input id="input" value={name} onChange={handleInput}></input>
+      <input id="input" ref={name} onChange={handleInput}></input>
+      {/* <input id="input" value={name} onChange={handleInput}></input> */}
       <button id="button" onClick={handleClick}>
         Click
       </button>
       <p id="text">
-        Hello my name is {finalName || "_______"} and I study at Newton School
-        {/* Hello my name is {finalName} and I study at Newton School */}
+        {/* Hello my name is {finalName || "_______"} and I study at Newton School */}
+        Hello my name is {finalName} and I study at Newton School
       </p>
     </div>
   );
