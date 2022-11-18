b8704ca3229cdfb41cfd5deca8626e3ead9c0dff
Nisha Tanwar <nishatanwar2706@gmail.com>
2022-11-18 19:35:38 +0530

    commit in p tag

--- src/components/App.js
+++ src/components/App.js
@@ -2,29 +2,24 @@ import React, { useState, useEffect ,useRef} from "react";
 import "../styles/App.css";
 const App = () => {
   //code here
-  // const [name , setName] = useState("");
   const name = useRef("");
   const [finalName, setFinalName] = useState("________");
 
   const handleInput = (e) => {
     name.current = e.target.value;
-    // setName(e.target.value);
   };
   const handleClick = () => {
     setFinalName(name.current);
-    // setFinalName(name);
   };
 
   return (
     <div id="main">
       <input id="input" ref={name} onChange={handleInput}></input>
-      {/* <input id="input" value={name} onChange={handleInput}></input> */}
       <button id="button" onClick={handleClick}>
         Click
       </button>
       <p id="text">
-        Hello my name is {finalName || "_______"} and I study at Newton School
-        {/* Hello my name is {finalName} and I study at Newton School */}
+        Hello my name is {finalName} and I study at Newton School
       </p>
     </div>
   );
